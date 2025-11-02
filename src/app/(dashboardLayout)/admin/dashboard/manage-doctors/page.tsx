a171655b6965508bcdc9e6e854068e6532a53b6a
MukterHossain <mukter3075@gmail.com>
2025-11-02 21:20:39 +0600

    add docor form

---
+++ src/app/(dashboardLayout)/admin/dashboard/manage-doctors/page.tsx
@@ -0,0 +1,11 @@
+import { DoctorTable } from "@/components/modules/Dashboard/ManageDoctor/DoctorTable";
+
+const ManageDoctors = () => {
+    return (
+        <>
+           <DoctorTable></DoctorTable> 
+        </>
+    );
+};
+
+export default ManageDoctors;
--- src/app/(dashboardLayout)/admin/dashboard/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/page.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 const DashboardPage = () => {
     return (
         <div>
-            <h1>Dashboard Page</h1>
             <DashboardComponent></DashboardComponent>
         </div>
     );
